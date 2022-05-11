39414d44e8f2b761a412fbccae45875eeddb11d2
xx745 <xx745>
2022-05-11 22:44:12 +0100

    added first example graph

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,37 @@
     <router-link to="/about">About</router-link>
   </nav>
   <router-view/>
+  <div class="ct-chart ct-perfect-fourth"></div>
 </template>
+
+<script>
+import Chartist from 'chartist';
+
+export default {
+  name: 'App',
+
+  mounted() {
+    const data = {
+      // A labels array that can contain any sort of values
+      labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
+      // Our series array that contains series objects or in this case series data arrays
+      series: [
+        [5, 2, 4, 2, 0]
+      ]
+    };
+
+    const options = {
+      width: 300,
+      height: 200
+    };
+
+    // Create a new line chart object where as first parameter we pass in a selector
+    // that is resolving to our chart container element. The Second parameter
+    // is the actual data object.
+    new Chartist.Line('.ct-chart', data, options);
+  }
+};
+</script>
 
 <style lang="scss">
 #app {
